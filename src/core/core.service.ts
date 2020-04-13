1e5d6c4d7d3e4e1f792b65d4e477ecf74a546072
peter0618 <peterchoi0618@gmail.com>
2020-04-14 02:54:47 +0900

    커밋테스트3

--- src/core/core.service.ts
+++ src/core/core.service.ts
@@ -6,6 +6,7 @@ export class CoreService {
   async getMoney(balance: number) {
     console.log("commit test!!");
     console.log("commit test2!!");
+    console.log("commit test3!!");
     return balance;
   }
 }
