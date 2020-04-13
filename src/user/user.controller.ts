3092a3e440d5aabaa5d980bc4b1190c41fafad22
peter <byungjoon.choi@solarconnect.kr>
2020-04-14 00:37:32 +0900

    User REST API 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async findAll() {
+    return await this.userService.findAllUsers();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) : Promise<User | { result: boolean; message: string }>{
+    return await this.userService.findOne(id);
+  }
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    this.userService.createUser(createUserDto);
+  }
+
+  @Patch()
+  async patchUser(@Body() user: User) {
+    this.userService.patchUser(user);
+  }
+
+  @Put()
+  async putUser(@Body() user: User) {
+    this.userService.putUser(user);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: number) {
+    console.log(id);
+    this.userService.deleteUser(id);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,66 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+  private users = [
+    new User(1, "Peter", 36),
+    new User(2, "Grace", 34),
+    new User(3, "Kevin", 27)
+  ];
+
+  async findAllUsers() {
+    console.log(this.users);
+    return this.users;
+  }
+
+  async findOne(id: number) {
+    for(const user of this.users){
+      console.log(user.id);
+      if(user.id == id) {
+        return user;
+      }
+    }
+    return {
+      result: false,
+      message: `There is no user has id = ${id}`
+    };
+  }
+
+  createUser(createUserDto: CreateUserDto) {
+    // TODO : users 에 id가 중복되지는 않는지 확인해야 합니다.
+    this.users.push(new User(createUserDto.id, createUserDto.name, createUserDto.age));
+    console.log(createUserDto);
+  }
+
+  patchUser(userReq: User) {
+    for(const user of this.users) {
+      if(user.id == userReq.id){
+        user.name = userReq.name ? userReq.name : user.name;
+        user.age = userReq.age ? userReq.age : user.age;
+      }
+    }
+  }
+
+  putUser(userReq: User) {
+    for(const user of this.users) {
+      if(user.id == userReq.id){
+        user.name = userReq.name;
+        user.age = userReq.age;
+      }
+    }
+  }
+
+  deleteUser(id: number) {
+    console.log(`delete id : ${id}`);
+    for(const user of this.users){
+      if(user.id == id) {
+        console.log(this.users.indexOf(user));
+        this.users.splice(this.users.indexOf(user), 1);
+        break;
+      }
+    }
+  }
+}
